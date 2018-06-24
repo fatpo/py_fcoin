e640604717c2394cdbe65b64f4c26567a65e6508
ouyangbro <xihuanjianguo@gmail.com>
2018-06-24 23:00:18 +0800

    init ...

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import time
 
 import fcoin
 
@@ -7,3 +8,23 @@ secret = ''
 
 api = fcoin.authorize(key, secret)
 print api.accounts_balance
+
+
+import fcoin
+from fcoin.WebsocketClient import WebsocketClient
+from threading import Thread
+class HandleWebsocket(WebsocketClient):
+    def handle(self,msg):
+        for key,value in msg.items():
+            print(key,value)
+ws = HandleWebsocket()
+topics = {
+         "id": "tickers",
+         "cmd": "sub",
+         "args": ["depth.L20.ethusdt"],
+    }
+sub = ws.sub
+Thread(target=sub,args=(topics,)).start()
+time.sleep(20)
+ws.close()
+
